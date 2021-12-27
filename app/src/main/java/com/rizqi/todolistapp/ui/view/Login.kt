91be748c352bd70392ea0587b7b1289842853412
Muhammad <mrizqi070502@gmail.com>
2021-12-27 13:04:04 +0700

    change status bar color

--- app/src/main/java/com/rizqi/todolistapp/ui/view/Login.kt
+++ app/src/main/java/com/rizqi/todolistapp/ui/view/Login.kt
@@ -1,6 +1,7 @@
 package com.rizqi.todolist.ui.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/com/rizqi/todolistapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/rizqi/todolistapp/ui/view/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.rizqi.todolist.ui.view
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.rizqi.todolist.nav.SetUpNavGraph
+import com.rizqi.todolistapp.ui.theme.Grey1
+import com.rizqi.todolistapp.ui.theme.Grey3
 import com.rizqi.todolistapp.ui.theme.ToDoListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,6 +21,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            window.statusBarColor = Grey3.hashCode()
+            window.navigationBarColor = Color.White.hashCode()
             ToDoListAppTheme {
                 // A surface container using the 'background' color from the theme
                 navHostController = rememberNavController()
